f816d5a0c11b4b6928e5764bae842ab86073a14f
Gabriel Pereira de Souza <gabrielsouza97@gmail.com>
2017-04-03 19:16:25 -0300

    Add files via upload

---
+++ exemploInterface/ControladorVenda.java
@@ -0,0 +1,17 @@
+package br.ufsc.ine5605.testeInterfaces;
+
+import java.util.ArrayList;
+
+public class ControladorVenda {
+
+	private ArrayList<Orcamento> orcamentos;
+	
+	public ControladorVenda() {
+		this.orcamentos = new ArrayList<>();
+	}
+	
+	public void iniciar() {
+		
+	}
+
+}
